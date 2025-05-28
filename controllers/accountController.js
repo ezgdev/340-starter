b944c5a6c490c8d8ddb8ebaf226c53742215ffd0
EzeG92 <gim24001@byui.edu>
2025-05-27 22:45:20 -0300

    register view

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -7,10 +7,22 @@ const accountController = {}
 * *************************************** */
 accountController.buildLogin = async function(req, res, next) {
     let nav = await utilities.getNav()
-    req.flash("notice", "Please log in to continue.")
+    // Uncomment line if you want to show a notice when the login page is accessed
+    // req.flash("notice", "Please log in to continue.") 
     res.render("account/login", {
         title: "Login",
         nav,
+    })
+}
+
+accountController.buildRegister = async function(req, res, next) {
+    let nav = await utilities.getNav()
+    // Uncomment line if you want to show a notice when the register page is accessed
+    // req.flash("notice", "Please complete all fields.") 
+    res.render("account/register", {
+        title: "Register",
+        nav,
+        errors: null
     })
 }
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,11 +3,12 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
 
-// route for the path that will be sent when the "My Account" link is clicked
-router.get("/account", utilities.handleErrors(accountController.buildMyAccount))
-
 // route for the path that will be sent when the "Login" link is clicked
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// route for the path that will be sent when the "Register" link is clicked
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
 
 module.exports = router;
 
